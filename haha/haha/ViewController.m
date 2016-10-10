b2a7975d367b361196e27c0042c159f7a93df12f
伟丰陈 <836594824@qq.com>
2016-10-10 18:32:47 +0800

    ddadasd

--- haha/haha/ViewController.m
+++ haha/haha/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
+    UIScrollView* scrolleV = [[UIScrollView alloc]init];
+    [self.view addSubview:scrolleV];
 }
 
 - (void)didReceiveMemoryWarning {
