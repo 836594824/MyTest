7e55bfa12492f12e2e6edf16f03df782963c4a3f
伟丰陈 <836594824@qq.com>
2016-10-10 18:19:08 +0800

    dddd

---
+++ haha/haha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  haha
+//
+//  Created by 伟丰陈 on 16/10/10.
+//  Copyright © 2016年 小陈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ haha/haha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  haha
+//
+//  Created by 伟丰陈 on 16/10/10.
+//  Copyright © 2016年 小陈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
